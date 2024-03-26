346984a69fe66d4fa07a5c43ca7d1da926d9ed87
davinahouquet <davina.houquet@hotmail.com>
2024-03-26 11:48:16 +0100

    [pierre_papier_ciseaux] Comptage et affichage du score

--- pierre_papier_ciseaux.py
+++ pierre_papier_ciseaux.py
@@ -1,25 +1,38 @@
 import random
 choix=['Pi','P','C']
-cpu=random.choice(choix)
+score_cpu=0
+score_joueur=0
 
 while True:
-    joueur = str(input('C: Ciseaux, Pi: Pierre ou P: Papier ? ')).capitalize()
+    cpu = random.choice(choix)
+    joueur = str(input('C: Ciseaux, Pi: Pierre ou P: Papier ? Pour terminer tapez Fin ')).capitalize()
     if joueur==cpu:
         print("Egalité !")
     elif joueur=='Pi':
         if cpu=='P':
             print("Vous perdez, le papier couvre la pierre")
+            score_cpu+=1
         elif cpu=='C':
             print("Vous gagnez, la pierre casse les ciseaux")
-    elif joueur=='Papier':
+            score_joueur+=1
+    elif joueur=='P':
         if cpu=='Pi':
             print('Vous gagnez, le papier couvre la pierre')
+            score_joueur += 1
         elif cpu=='C':
             print('Vous perdez, les ciseaux coupent le papier')
+            score_cpu += 1
     elif joueur=='C':
         if cpu=='Pi':
             print('Vous perdez, la pierre casse les ciseaux')
-        elif cpu=='C':
+            score_cpu += 1
+        elif cpu=='P':
             print('Vous gagnez, les ciseaux coupent le papier')
+            score_joueur += 1
+    elif joueur=='Fin':
+        print('Résultat : ')
+        print(f"CPU : {score_cpu}")
+        print(f"joueur : {score_joueur}")
+        break
     else:
         print("Je n'ai pas compris, vérifiez l'orthographe")
