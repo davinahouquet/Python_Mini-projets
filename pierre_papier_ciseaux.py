3454dcc21bdcd19f19e92d4214278f4d19b58279
davinahouquet <davina.houquet@hotmail.com>
2024-03-26 11:37:50 +0100

    [pierre_papier_ciseaux] Condition en cas de faute de frappe

--- pierre_papier_ciseaux.py
+++ pierre_papier_ciseaux.py
@@ -1,21 +1,25 @@
 import random
 choix=['Pi','P','C']
 cpu=random.choice(choix)
-joueur=str(input('C: Ciseaux, Pi: Pierre ou P: Papier ? ')).capitalize()
-if joueur==cpu:
-    print("Egalité !")
-elif joueur=='Pi':
-    if cpu=='P':
-        print("Vous perdez, le papier couvre la pierre")
-    elif cpu=='C':
-        print("Vous gagnez, la pierre casse les ciseaux")
-elif joueur=='Papier':
-    if cpu=='Pi':
-        print('Vous gagnez, le papier couvre la pierre')
-    elif cpu=='C':
-        print('Vous perdez, les ciseaux coupent le papier')
-elif joueur=='C':
-    if cpu=='Pi':
-        print('Vous perdez, la pierre casse les ciseaux')
-    elif cpu=='C':
-        print('Vous gagnez, les ciseaux coupent le papier')
+
+while True:
+    joueur = str(input('C: Ciseaux, Pi: Pierre ou P: Papier ? ')).capitalize()
+    if joueur==cpu:
+        print("Egalité !")
+    elif joueur=='Pi':
+        if cpu=='P':
+            print("Vous perdez, le papier couvre la pierre")
+        elif cpu=='C':
+            print("Vous gagnez, la pierre casse les ciseaux")
+    elif joueur=='Papier':
+        if cpu=='Pi':
+            print('Vous gagnez, le papier couvre la pierre')
+        elif cpu=='C':
+            print('Vous perdez, les ciseaux coupent le papier')
+    elif joueur=='C':
+        if cpu=='Pi':
+            print('Vous perdez, la pierre casse les ciseaux')
+        elif cpu=='C':
+            print('Vous gagnez, les ciseaux coupent le papier')
+    else:
+        print("Je n'ai pas compris, vérifiez l'orthographe")
