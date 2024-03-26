3fb179d1ff25a53cae1cea39a0f88d40946d46d5
davinahouquet <davina.houquet@hotmail.com>
2024-03-26 11:12:10 +0100

    [generateur_mdp] Définition des caractères

--- generateur_mot_passe.py
+++ generateur_mot_passe.py
@@ -2,3 +2,5 @@ import random
 
 longpass=int(input("Donner la longueur du mot de passe:"))
 
+s='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&()*+,-./:;<=>?@[]^_`{|}~'
+print(random.sample(s,longpass))
