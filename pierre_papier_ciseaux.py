b04c6e4a427d1db56d33f97144668cba7a4b80a3
davinahouquet <davina.houquet@hotmail.com>
2024-03-26 11:34:08 +0100

    [pierre_papier_ciseaux] Conditions de jeux

--- pierre_papier_ciseaux.py
+++ pierre_papier_ciseaux.py
@@ -1,4 +1,21 @@
 import random
-choix=['Pierre','Papier','Ciseaux']
+choix=['Pi','P','C']
 cpu=random.choice(choix)
 joueur=str(input('C: Ciseaux, Pi: Pierre ou P: Papier ? ')).capitalize()
+if joueur==cpu:
+    print("Egalité !")
+elif joueur=='Pi':
+    if cpu=='P':
+        print("Vous perdez, le papier couvre la pierre")
+    elif cpu=='C':
+        print("Vous gagnez, la pierre casse les ciseaux")
+elif joueur=='Papier':
+    if cpu=='Pi':
+        print('Vous gagnez, le papier couvre la pierre')
+    elif cpu=='C':
+        print('Vous perdez, les ciseaux coupent le papier')
+elif joueur=='C':
+    if cpu=='Pi':
+        print('Vous perdez, la pierre casse les ciseaux')
+    elif cpu=='C':
+        print('Vous gagnez, les ciseaux coupent le papier')
