8c534ac50c8bcf23b902dc719e4d71446c57562a
davinahouquet <davina.houquet@hotmail.com>
2024-03-26 10:59:41 +0100

    [lancer_de] Conditions de x entier

--- lancer_de.py
+++ lancer_de.py
@@ -1,4 +1,8 @@
 import random
 while True:
-    input("Cliquer sur un bouton ")
-    print(random.randint(1,6)) #Génère des entiers entre a et b
+    x=int(input("Cliquer sur un bouton ")) #x est forcément un entier
+    if x==0:
+        print('Terminé')
+        break
+    elif x==1:
+        print(random.randint(1,6)) #Génère des entiers entre a et b
