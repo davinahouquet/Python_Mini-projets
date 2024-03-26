47f7fea6320b2aa90d4ba17507b09997979db4fb
davinahouquet <davina.houquet@hotmail.com>
2024-03-26 10:55:45 +0100

    [lancer_de] Ajout boucle while

--- lancer_de.py
+++ lancer_de.py
@@ -1,3 +1,4 @@
 import random
-input("CLiquer sur un bouton")
-print(random.randint(1,6)) #Génère des entiers entre a et b
+while True:
+    input("Cliquer sur un bouton ")
+    print(random.randint(1,6)) #Génère des entiers entre a et b
