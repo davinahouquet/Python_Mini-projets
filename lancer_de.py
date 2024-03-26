965c4a33186c080c08ea425b2691e372773557ea
davinahouquet <davina.houquet@hotmail.com>
2024-03-26 11:03:45 +0100

    [lancer_de] Consignes d'utilisation et gestion des erreurs

--- lancer_de.py
+++ lancer_de.py
@@ -1,8 +1,11 @@
 import random
+print("1: Lancer le dé, 0: Quitter le programme")
 while True:
     x=int(input("Cliquer sur un bouton ")) #x est forcément un entier
     if x==0:
         print('Terminé')
         break
     elif x==1:
-        print(random.randint(1,6)) #Génère des entiers entre a et b
+        print(random.randint(1,80)) #Génère des entiers entre a et b
+    else:
+        print("Je n'ai pas compris")
